f222de5423064d5a4c8caaad9cf193b561b9ef25
hudalei001 <1169623152@qq.com>
2022-09-30 11:49:51 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //React.StrictMode:严格模式, 注意: 严格模式检查只在开发模式下运行，不会与生产模式冲突
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.warn);
